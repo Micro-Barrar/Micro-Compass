a20d0166befa2ef25ff29b3735b80f73440943ee
Micro-Barrar <fariasbenjamin104@gmail.com>
2024-08-01 08:52:40 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,44 @@
+basic.forever(function () {
+    if (input.compassHeading() == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() == -90) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() == 180) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() == 90) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
